22bd3a60794787d217ba6ac3dfa9207dc43dfe59
Griffin McCormick <mccormickgriffin@gmail.com>
2023-11-24 18:04:11 -0500

    added moving to created directory and initializing a git repo

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import subprocess
+import os
 
+# Get user input for what word they want to write on what years contribution graph
 year = input("What year would you like to write on? ")
 word = input("What would you like to write? ")
 
-subprocess.run(["mkdir", "{}-{}".format(year, word)])
+# Create new folder for generated git repo
+folder_name = f"{year}-{word}"
+create_folder_command = f"mkdir {folder_name}"
+process = subprocess.Popen(create_folder_command,
+                           shell=True,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.PIPE,
+                           text=True)
+output, error = process.communicate()
+
+if process.returncode == 0:
+    os.chdir(folder_name)
+    subprocess.run(["git", "init"])
+else:
+    print(f"Error creating folder: {error}")
  
