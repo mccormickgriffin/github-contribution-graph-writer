f80351ff87730e5dcde01d28d2a972c38e7c61dc
Griffin McCormick <mccormickgriffin@gmail.com>
2023-11-24 22:32:01 -0500

    added a commit for every day of the specified year

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import subprocess
 import os
+from datetime import datetime, timedelta
 
-# Get user input for what word they want to write on what years contribution graph
-year = input("What year would you like to write on? ")
-word = input("What would you like to write? ")
+COMMIT_FILE = "commit.md"
 
-# Create new folder for generated git repo
-folder_name = f"{year}-{word}"
-create_folder_command = f"mkdir {folder_name}"
-process = subprocess.Popen(create_folder_command,
-                           shell=True,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE,
-                           text=True)
-output, error = process.communicate()
+def make_git_commit(commit_date):
+    formatted_date = commit_date.strftime("%Y-%m-%d %H:%M:%S")
+    with open(COMMIT_FILE, 'w') as file:
+        file.write(formatted_date)
+    os.environ['GIT_COMMITTER_DATE'] = formatted_date
+    os.environ['GIT_AUTHOR_DATE'] = formatted_date
+    subprocess.run(["git", "add", COMMIT_FILE, "-f"])
+    subprocess.run(["git", "commit", "--date", f'"{formatted_date}"', "-m", f'"Commit for {formatted_date}"'])
 
-if process.returncode == 0:
-    os.chdir(folder_name)
-    subprocess.run(["git", "init"])
-else:
-    print(f"Error creating folder: {error}")
- 
+def main():
+    # Get user input for what word they want to write on what years contribution graph
+    year = int(input("What year would you like to write on? "))
+    word = input("What would you like to write? ")
+
+    # Create new folder for generated git repo
+    folder_name = f"gcgw-{year}-{word}"
+    create_folder_command = f"mkdir {folder_name}"
+    process = subprocess.Popen(create_folder_command,
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            text=True)
+    output, error = process.communicate()
+    print(output)
+
+    if process.returncode == 0:
+        os.chdir(folder_name)
+        subprocess.run(["git", "init"])
+        with open(COMMIT_FILE, 'w'):
+            pass
+
+        start_date = datetime(year, 1, 1, 12, 0, 0)
+        end_date = datetime(year, 12, 31, 12, 0, 0)
+        current_date = start_date
+        while current_date <= end_date:
+            make_git_commit(current_date)
+            current_date += timedelta(days=1)
+    else:
+        print(f"Error creating folder: {error}")
+
+if __name__ == "__main__":
+    main()
