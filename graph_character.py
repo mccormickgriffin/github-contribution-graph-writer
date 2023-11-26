a96ad15858f2700accb7323001cae5e1635a5d13
Griffin McCormick <mccormickgriffin@gmail.com>
2023-11-26 13:22:30 -0500

    added += operator to GraphCharacter

--- graph_character.py
+++ graph_character.py
@@ -1,8 +1,22 @@
+ROW_COUNT = 7
+
 class GraphCharacter:
     def __init__(self, graph) -> None:
-        if (len(graph) != 7):
+        if (len(graph) != ROW_COUNT):
             raise ValueError("Invalid graph character height")
         self.graph = graph
 
     def width(self):
         return len(self.graph[0])
+    
+    def __iadd__(self, other):
+        if not isinstance(other, GraphCharacter):
+            raise ValueError(f"Cannot add GraphCharacter with object of type: {type(other)}")
+        
+        for i in range(ROW_COUNT):
+            # Add a blank space for between characters
+            self.graph[i] += [0]
+            # Concatinate the other letter
+            self.graph[i] += other.graph[i]
+        
+        return self
