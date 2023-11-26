c517ebe8ee5a50551797fc6f0ea02a726892c499
Griffin McCormick <mccormickgriffin@gmail.com>
2023-11-26 12:58:52 -0500

    added error for creating an empty word

--- graph_word.py
+++ graph_word.py
@@ -2,6 +2,9 @@ from graph_characters import graph_characters
 
 class GraphWord:
     def __init__(self, word) -> None:
+        if len(word) == 0:
+            raise ValueError("Cannot create a GraphWord of zero length")
+
         unsupported_characters = set(word) - set(graph_characters.keys())
         if unsupported_characters:
             raise NotImplementedError(f"Character(s) [{', '.join(unsupported_characters)}] not implemented yet")
