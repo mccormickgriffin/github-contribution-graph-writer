4dd5c8f25647bea28c35d7e850fad5b1233fa46f
Griffin McCormick <mccormickgriffin@gmail.com>
2023-11-26 13:24:15 -0500

    implemented width and initializing the graph string for GraphWord class

--- graph_word.py
+++ graph_word.py
@@ -1,12 +1,20 @@
-from graph_characters import graph_characters
+from graph_characters import character_to_graph_mapping
+from graph_character import GraphCharacter
 
 class GraphWord:
     def __init__(self, word) -> None:
         if len(word) == 0:
             raise ValueError("Cannot create a GraphWord of zero length")
 
-        unsupported_characters = set(word) - set(graph_characters.keys())
+        unsupported_characters = set(word) - set(character_to_graph_mapping.keys())
         if unsupported_characters:
             raise NotImplementedError(f"Character(s) [{', '.join(unsupported_characters)}] not implemented yet")
         
+        self.graph_string = GraphCharacter(character_to_graph_mapping[word[0]])
+        for i in range(1, len(word)):
+            self.graph_string += GraphCharacter(character_to_graph_mapping[word[i]])
+
         self.word = word
+
+    def width(self):
+        return self.graph_string.width()
